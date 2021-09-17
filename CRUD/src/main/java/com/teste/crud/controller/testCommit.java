5d258755b76d4081e0e90a26a3b2b9c0ecdfb618
DanielDASH <danielincaio19@gmail.com>
2021-09-17 20:32:47 -0300

    testando vs

--- CRUD/src/main/java/com/teste/crud/controller/testCommit.java
+++ CRUD/src/main/java/com/teste/crud/controller/testCommit.java
@@ -3,7 +3,5 @@ package com.teste.crud.controller;
 public class testCommit {
 	private String teste;
 	private String outroTeste;
-	private int idade;
-	private int ano;
-
+	private String testeNew;
 }
